8697a145cec946f73592769a271f88a2ee2bd47d
Arman7052 <99arman7052@gmail.com>
2023-05-03 10:05:33 +0600

    first commit

---
+++ check.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors= require('cors')
+const port = process.env.PORT || 7052;
+app.use(cors())
+const chefRecipe= require('./data/chef-recipe.json');
+
+app.get('/', (req, res) => {
+    res.send('Taste Bengal is running')
+})
+
+app.get('/chefrecipe', (req, res) => {
+    res.send(chefRecipe);
+})
+
+app.get('/chefrecipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const viewRecipes = chefRecipe.find(n => n.id == id);
+    res.send(viewRecipes)
+})
+app.listen(port, () => {
+    console.log(`Taste Bengal is running on port: ${port}`)
+})
