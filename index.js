2461e26352653134b8751694f37d6bd0c12f11cc
Arman7052 <99arman7052@gmail.com>
2023-05-03 10:19:15 +0600

    Json to Api

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const port = process.env.PORT || 7052;
 const cors = require('cors')
 app.use(cors())
 
+const chefRecipe= require('./data/chef-recipe.json');
+
 app.get('/', (req, res) => {
     res.send('Taste Bengal is running')
 })
 
-
-
-
+app.get('/chefrecipe', (req, res) => {
+    res.send(chefRecipe);
+})
 
 
 
