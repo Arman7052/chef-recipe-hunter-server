740939ba417b2e80d72a6118d5bb427b2e917988
Arman7052 <99arman7052@gmail.com>
2023-05-03 10:14:52 +0600

    Cors added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 7052;
-
+const cors = require('cors')
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Taste Bengal is running')
